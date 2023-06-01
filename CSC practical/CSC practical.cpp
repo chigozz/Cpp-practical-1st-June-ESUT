e52d0e439fb4a98632c26434158887545279c399
Chigozzdev <chigozzdev@gmail.com>
2023-06-01 13:10:31 +0100

    Add project files.

---
+++ CSC practical/CSC practical.cpp
@@ -0,0 +1,18 @@
+// CSC practical.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	cout << "Hello wORLD!" << endl;
+
+	cout << "Welcome to CSC 215 practical" << endl;
+
+
+
+
+	return 0;
+}
